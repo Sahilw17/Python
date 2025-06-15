b95919f7950cb2c0398f55237506f05a21310bc3
Sahilw17 <sahilwaghole1784@gmail.com>
2025-06-15 12:59:38 +0530

    client.py

--- Mproject1/client.py
+++ Mproject1/client.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 client = OpenAI(
-    api_key="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"
+    api_key="Open_ai_apikey"
 )
 
 completion = client.chat.completions.create(
